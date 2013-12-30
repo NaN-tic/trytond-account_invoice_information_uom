e9ac5b4b503758d288346eae599771860b2fecb7
?ngel ?lvarez <angel@nan-tic.com>
2013-12-30 15:57:45 +0100

    fix and tests

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 
 from trytond.pool import Pool
 from template import *
+from invoice import *
+
 
 def register():
     Pool.register(
         Template,
+        InvoiceLine,
         module='account_invoice_information_uom', type_='model')
--- invoice.py
+++ invoice.py
@@ -1,21 +1,24 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import datetime
 
-from trytond.model import ModelView, ModelSQL, fields
+from trytond.model import fields
 from trytond.pyson import Eval, If
-from trytond.pool import Pool, PoolMeta
-from trytond.transaction import Transaction
-from trytond import backend
+from trytond.pool import PoolMeta
 from decimal import Decimal
+
+__all__ = ['InvoiceLine']
+__metaclass__ = PoolMeta
+
+_ZERO = Decimal('0.0')
+_ROUND = Decimal('.0001')
 
 
 class InvoiceLine:
-    __name__ = 'account.invoce.line'
+    __name__ = 'account.invoice.line'
 
     info_unit = fields.Function(fields.Many2One('product.uom',
             'Information UOM',
-            on_change_with=['product']), 'get_info_unit')
+            on_change_with=['product']), 'on_change_with_info_unit')
     info_unit_digits = fields.Function(fields.Integer(
         'Information Unit Digits', on_change_with=['info_unit']),
         'on_change_with_unit_digits')
@@ -26,18 +29,22 @@ class InvoiceLine:
             'invisible': Eval('type') != 'line',
             'required': Eval('type') == 'line',
             },
-        on_change_with=['quantity', 'product', 'info_quantity'],
-        on_change=['quantity', 'unit_quantity', 'info_quantity']
-        depends=['type', 'info_unit_digits'])
+        on_change_with=['quantity', 'product', 'info_quantity',
+            'info_unit_price', 'product'],
+        on_change=['quantity', 'unit_quantity', 'unit',
+            'info_quantity', 'info_unit_price', 'product'],
+        depends=['type', 'info_unit_digits', 'product', 'unit', 'unit_digits'])
 
     info_unit_price = fields.Numeric('Information Unit Price', digits=(16, 4),
         states={
             'invisible': Eval('type') != 'line',
             'required': Eval('type') == 'line',
             },
-        on_change=['unit_price', 'invoice_type']
-        on_change_with=['unit_price', 'type', 'invoice_type',  'product'],
-        depends=['type', 'invoice_type'])
+        on_change=['info_unit_price', 'invoice_type', 'product', 'info_quantity',
+            'invoice_type', 'type'],
+        on_change_with=['unit_price', 'type', 'invoice_type',
+            'info_unit_price', 'info_quantity', 'product', 'unit_price'],
+        depends=['type', 'invoice_type', 'product'])
 
     info_amount = fields.Function(fields.Numeric('Information Amount',
             digits=(16, Eval('_parent_invoice', {}).get('currency_digits',
@@ -45,14 +52,9 @@ class InvoiceLine:
             states={
                 'invisible': ~Eval('type').in_(['line', 'subtotal']),
                 },
-            on_change_with=['type', 'quantity', 'unit_price', 'info_quantity',
-                '_parent_invoice.currency', 'currency'],
+            on_change_with=['type', 'info_unit_price',
+                'info_quantity', '_parent_invoice.currency', 'currency'],
             depends=['type', 'currency_digits']), 'get_amount')
-
-    def get_info_unit(self):
-        if self.product and self.product.use_info_unit:
-            return self.product.info_unit.id
-        return None
 
     @staticmethod
     def default_info_unit_digits():
@@ -64,31 +66,43 @@ class InvoiceLine:
         return 2
 
     def on_change_with_info_unit(self, name=None):
-        if not self.product or not self.product.use_info_unit:
-            return None
-        return self.product.info_unit.id
+        if (self.product and self.product.use_info_unit and
+                self.product.info_unit):
+            return self.product.info_unit.id
+        return None
 
     def on_change_with_info_quantity(self, name=None):
-        if not self.product:
+        if not self.product or not self.quantity:
             return
-        return self.product.calc_info_quantity(self.qty)
+        return self.product.calc_info_quantity(self.quantity)
 
     def on_change_info_quantity(self, name=None):
         if not self.product:
             return {}
+        qty = self.product.calc_quantity(
+                self.info_quantity, self.unit)        
         return {
-            'qty': self.product.calc_info_quantity(self.info_qty, self.unit)
+            'quantity': float(qty)
             }
 
     def on_change_with_info_unit_price(self, name=None):
         if not self.product:
             return
-        return self.product.get_unit_price(self.info_unit_price)
+        if not self.unit_price:
+            return
+        if self.type in ('out_invoice', 'out_refund'):
+            return self.product.get_info_list_price(self.unit_price)
+        else:
+            return self.product.get_info_cost_price(self.unit_price)
 
-    def on_change_info_unit_price(self, name=None):
+    def on_change_info_unit_price(self, name=None):        
         if not self.product:
             return {}
+        if not self.info_unit_price:
+            return {}
         return {
-            'qty': self.product.calc_info_quantity(self.info_qty, self.unit)
+            'unit_price': self.product.get_unit_price(self.info_unit_price)
             }
 
+    def on_change_with_info_amount(self, name=None):
+        return self.info_unit_price * self.info_quantity
