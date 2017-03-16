31725c2606239e30efadd0a826dc86eaaf0983ce
Raimon Esteve <raimon@nan-tic.com>
2017-03-16 14:26:10 +0100

    Upgrade 3.8

--- __init__.py
+++ __init__.py
@@ -1,13 +1,12 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from trytond.pool import Pool
-from template import *
-from invoice import *
+from . import template
+from . import invoice
 
 
 def register():
     Pool.register(
-        Template,
-        InvoiceLine,
+        template.Template,
+        invoice.InvoiceLine,
         module='account_invoice_information_uom', type_='model')
