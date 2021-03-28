e16c0641b673fb4b59e101405d7d65f0128e5c69
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-28 01:02:39 +0100

    Pyflakes fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from .test_account_invoice_information_uom import suite
+
+__all__ = ['suite']
